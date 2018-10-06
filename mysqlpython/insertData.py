7cb5a31b582a8ea85576df3d90ea5d4909b3a3a0
Nikunj Jhaveri <nikunjjhaveri@gmail.com>
2018-10-06 07:06:12 -0500

    Add files via upload

--- mysqlpython/insertData.py
+++ mysqlpython/insertData.py
@@ -13,21 +13,22 @@ conn = mysql.connect()
 cursor =conn.cursor()
 
 def insert_personal(args):
-    query = "Insert into Personal(email, first_name, last_name, dob, ssn) VALUES(%s, %s, %s, %s, %s)"
-    values = (args["Email"], args[])
+    query = "Insert into Personal(email, first_name, middle_name, last_name, dob, ssn) VALUES(%s, %s, %s, %s, %s)"
+    values = (args["Email"], args["First Name"], args["Middle Name"], args["Last Name"], args["DOB"], args["SSN]"])
     cursor.execute(query, values)
 
-def insert_Contact_Info(args):
+def insert_Contact_Info(email, args):
     query = "Insert into Contact_Info(email, address, city, state, postal_code, county, phone) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    values = (email, args["Street Address"], args["City"], args["State"], args["Postal Code"], args["County"], args["Phone"])
     cursor.execute(query, args)
 
-def insert_Demographic_Info(args):
-    query = "Insert into Demographic_Info(email, male, female, white, black, asian, hispanic, hnpi, other_race, african, chinese, german, japenese, persoan, russian, vietnamese, na, arabic, french, italian, korean, portuguese, spanish, other_languages) VALUES(%s, %{args[""]})"
+def insert_Demographic_Info(email, args):
+    query = "Insert into Demographic_Info(email, male, female, white, black, asian, hispanic, hnpi, other_race, african, chinese, german, japenese, persoan, russian, vietnamese, na, arabic, french, italian, korean, portuguese, spanish, other_languages) VALUES(%{email}, %{args[""]})"
     cursor.execute(query,args)
 
 def insert_Work_Auth(args):
     query = "Insert into Work_Auth(email, citizenship, noncitizenship, valid_id, transportation, homeowner) VALUES()"
-    cursor.execute(query,agrs)
+    cursor.execute(query,args)
 
 def insert_Youth(args):
     query = "Insert into Youth(email, foster, p_incarcerate, juvie, p_single, fr_lunch, drop_out, parent, lack_work) VALUES()"
