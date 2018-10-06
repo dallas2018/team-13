7efd1b792134346c4c85861d7d143ceb5a31ba23
DESKTOP-J41FMKN\Mark <mark.drobnjak@gmail.com>
2018-10-06 04:59:49 -0400

    got static html links working

---
+++ SOURCE/flaskApp/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/1/')
+def one():
+    return render_template('1.html')
+
+
+if __name__ == "__main__":
+    app.run()
+
