bc36cd175977649a2e70f67a52a62d59a49a1b8e
Nikunj Jhaveri <nikunjjhaveri@gmail.com>
2018-10-06 03:51:39 -0500

    Add files via upload

--- mysqlpython/insertData.py
+++ mysqlpython/insertData.py
@@ -13,11 +13,19 @@ conn = mysql.connect()
 cursor =conn.cursor()
 
 def insert_personal(args):
-    query = "Insert into personal(email, first_name, last_name, dob, ssn) VALUES(%s, %s, %s, %s, %s)"
-    cursor.execute(query,args)
+    query = "Insert into Personal(email, first_name, last_name, dob, ssn) VALUES(%s, %s, %s, %s, %s)"
+    values = (args["Email"], args[])
+    cursor.execute(query, values)
 
 def insert_Contact_Info(args):
     query = "Insert into Contact_Info(email, address, city, state, postal_code, county, phone) VALUES(%s, %s, %s, %s, %s, %s, %s)"
     cursor.execute(query, args)
 
 def insert_Demographic_Info(args):
+    query = "Insert into Demographic_Info(email, male, female, white, black, asian, hispanic, hnpi, other_race, african, chinese, german, japenese, persoan, russian, vietnamese, na, arabic, french, italian, korean, portuguese, spanish, other_languages) VALUES(%s, %{args[""]})"
+    cursor.execute(query,args)
+
+def insert_Work_Auth(args):
+    query = "Insert into Work_Auth(citizenship, noncitizenship, valid_id, transportation, homeowner) VALUES()"
+    cursor.execute(query,agrs)
+    
