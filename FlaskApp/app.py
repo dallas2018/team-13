816926bd7d31155fe6dcd62e382ce1252a9e3e6d
Rahul Pant <rahulpant1997@.com>
2018-10-06 02:13:40 -0500

    merged

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-<<<<<<< HEAD
-=======
-app.debug = True
->>>>>>> 851f534dcbc55be73ee1a473cbf8baa48a9114bf
 @app.route("/home")
 def main():
     return render_template('index.html')
@@ -47,7 +43,6 @@ def appt():
     avail = request.form['Availability']
     contactPref = request.form['Contact Preference']
 
-<<<<<<< HEAD
 @app.route('/household',method=['POST'])
 def household():
     marital_status = request.form['Marital Status']
@@ -55,35 +50,27 @@ def household():
     youngAdults = request.form['Young Adults 18-24']
     adults = request.form['Adults']
 
+@app.route('/finance',method=['POST'])
+def finance():
+    checking = request.form['Checking']
+    saving = request.form['Saving']
+    paydayLoan = request.form['Payday Loan']
+    carTitle = request.form['Car Title Loan']
+    householdIncome = request.form['Household Income']
+    publicAssistance = request.form['Public Assistance Benefits Programs']
+
+@app.route('/clientBackground',method=['POST'])
+def clientBackground():
+    educationLevel = request.form['Highest Level of Education']
+    currentlyAttending = request.form['Currently Attending School']
+    paydayLoan = request.form['Payday Loan']
+    computerSkills = request.form['Computer Skills']
+    englishLiteracy = request.form['English Literacy Level']
+    certs = request.form['Certificates/Training']
+    militaryStatus = request.form['Military Status']
+    criminal = request.form['Criminal Status']
+
 
 
 if __name__ == "__main__":
     app.run()
-=======
-if __name__ == "__main__":
-    app.run()
-@app.route("/")
-def render():
-    return render_template("index.1.html")
-
-@app.route("/api/<path:path>", methods=['GET', 'POST'])
-def process_form(path):
-    if request.method == 'POST':
-        if path == "1":
-            _firstname = request.form['firstname']
-            _lastname = request.form['lastname']
-            print(_firstname, _lastname)
-            return render_template("index.2.html")
-        elif path == "2":
-            print("This is of form 2")
-        elif path == "3":
-            print("This is of form 3")
-        else:
-            print("We can't accept anything not of path num")
-    else:
-        print("REQUEST IS NOT POST, UNHANDLED")
-
-
-if __name__ == "__main__":
-    app.run('127.0.0.1', '8080')
->>>>>>> 851f534dcbc55be73ee1a473cbf8baa48a9114bf
