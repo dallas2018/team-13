d7e20c668b3bde387150b68ebd632af5d1b81da5
DESKTOP-J41FMKN\Mark <mark.drobnjak@gmail.com>
2018-10-06 03:00:47 -0400

    mark's initial commit - sms/email scripts
    automated sms code is working, automated email code is non-functional, some test html/css code is functional

---
+++ mark scripts/flaskApp/send_sms.py
@@ -0,0 +1,12 @@
+# we import the Twilio client from the dependency we just installed
+from twilio.rest import Client
+
+# the following line needs your Twilio Account SID and Auth Token
+client = Client()
+
+# change the "from_" number to your Twilio number and the "to" number
+# to the phone number you signed up for Twilio with, or upgrade your
+# account to send SMS to any phone number
+client.messages.create(to="+16313984501",
+                       from_="+16312142113",
+                       body="Thank you for your interest in SER Houston. blah blah blah")
