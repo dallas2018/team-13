de072b010b8c9fda44430503be9c8c27697e2578
Nikunj Jhaveri <nikunjjhaveri@gmail.com>
2018-10-06 05:23:50 -0500

    created main format to insert tables

--- mysqlpython/insertData.py
+++ mysqlpython/insertData.py
@@ -26,6 +26,36 @@ def insert_Demographic_Info(args):
     cursor.execute(query,args)
 
 def insert_Work_Auth(args):
-    query = "Insert into Work_Auth(citizenship, noncitizenship, valid_id, transportation, homeowner) VALUES()"
+    query = "Insert into Work_Auth(email, citizenship, noncitizenship, valid_id, transportation, homeowner) VALUES()"
     cursor.execute(query,agrs)
-    
+
+def insert_Youth(args):
+    query = "Insert into Youth(email, foster, p_incarcerate, juvie, p_single, fr_lunch, drop_out, parent, lack_work) VALUES()"
+    cursor.execute(query,args)
+
+def insert_Appointment(args):
+    query = ""
+
+def insert_Household(args):
+    query = "Inserts into Household(email, marital_status, children, young_adults, adults) VALUES()"
+    cursor.execute(query,args)
+
+def insert_Finance(args):
+    query = "Insert into Finance(email, checking, savings, payday, car_title, income, public_assist) VALUES()"
+    cursor.execute(query,args)
+
+def insert_Background(args):
+    query = "Insert into Background(email, education, attending_school, computer, english, certificated, military, convicted, incarcerated)"
+    cursor.execute(query,args)
+
+def insert_Job(job,args):
+    query = "Insert into %{job}(email, title, employer, job_type, commitment, start, end_date, wage, hours. supervis. address, reason) VALUES(),"
+    cursor.execute(query,args)
+
+def insert_Needs(args):
+    query = "Insert into Needs(email, interests, availability, investment, getting_job, wage_exp, drug) VALUES()"
+    cursor.execute(query, args)
+
+def insert_Questions(args):
+    query = "Insert into Questions(email, church_flyer, church_presentation, community_flyer, community_agency, guidance counselor, hcc, information_session, interet, job_fair, ser_client, united_way, workforce) VALUES()"
+    cursor.execute(query, args)
