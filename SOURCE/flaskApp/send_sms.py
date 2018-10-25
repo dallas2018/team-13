34ab6b59501228805dfb3bb9cff79095d55932d6
Luke Harrold <lukevharrold@gmail.com>
2018-10-25 18:03:19 +0100

    update

--- SOURCE/flaskApp/send_sms.py
+++ SOURCE/flaskApp/send_sms.py
@@ -2,7 +2,7 @@
 from twilio.rest import Client
 
 # the following line needs your Twilio Account SID and Auth Token
-client = Client()
+client = Client("","")
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
